82a8573576a59006a3ba0b99cb6405a5119d8c8f
Abbas Naqvi <sma.naqvi@outlook.com>
2022-02-28 23:27:16 +0000

    user model completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :donees
+  has_many :donations
+  has_one  :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
