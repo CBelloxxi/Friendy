922fd3368cd606162eb7999f066b3a492c6e7b8b
Abbas Naqvi <sma.naqvi@outlook.com>
2022-03-01 12:53:31 +0000

    report completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_124306) do
+ActiveRecord::Schema.define(version: 2022_03_01_125214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,20 @@ ActiveRecord::Schema.define(version: 2022_03_01_124306) do
     t.index ["user_id"], name: "index_donees_on_user_id"
   end
 
+  create_table "reports", force: :cascade do |t|
+    t.text "message"
+    t.string "assistance_type"
+    t.string "location"
+    t.float "latitude"
+    t.float "longitude"
+    t.bigint "user_id", null: false
+    t.bigint "donee_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["donee_id"], name: "index_reports_on_donee_id"
+    t.index ["user_id"], name: "index_reports_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,4 +67,6 @@ ActiveRecord::Schema.define(version: 2022_03_01_124306) do
   add_foreign_key "donations", "donees"
   add_foreign_key "donations", "users"
   add_foreign_key "donees", "users"
+  add_foreign_key "reports", "donees"
+  add_foreign_key "reports", "users"
 end
