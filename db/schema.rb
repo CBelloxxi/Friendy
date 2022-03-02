d6c2f8ea0a83dcae4037b6ef2166824905b5662d
Christopher Bellocci <christopher.bellocci@hotmail.com>
2022-03-02 12:04:36 +0000

    heroku, form location, location migration, cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_125214) do
+ActiveRecord::Schema.define(version: 2022_03_02_114919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_03_01_125214) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "location"
     t.index ["user_id"], name: "index_donees_on_user_id"
   end
 
