6d5f4d3781468938720bbc547c5d301bd8b04630
Christopher Bellocci <christopher.bellocci@hotmail.com>
2022-03-02 11:00:31 +0000

    changes for heroku

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,6 @@ class Report < ApplicationRecord
   belongs_to :donee
 
   validates :message, :assistance_type, :location, presence: true
-  geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_location?
+  # geocoded_by :location
+  # after_validation :geocode, if: :will_save_change_to_location?
 end
