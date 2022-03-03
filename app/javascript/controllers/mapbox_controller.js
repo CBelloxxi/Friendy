3db3af01394c2ad4a4385e875c9c26335f9828cc
Thinh Tran <thinh2000@icloud.com>
2022-03-03 16:27:19 +0000

    smashed it

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -17,6 +17,8 @@ export default class extends Controller {
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }))
 
   }
   #addMarkersToMap() {
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :donees do
     resources :donations, only: %i[new create show]
+    resources :reports, only: %i[new create]
   end
-  resources :reports, only: %i[new create edit update show]
+  resources :reports, only: %i[show]
 end
