56e03a45ec55c3780dbd5b9c57d77345e3371c62
joe mcilroy <joeamcilroy@gmail.com>
2022-03-03 13:16:05 +0000

    style updates impact and home page separated

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   get 'dashboard', to: 'pages#index', as: 'dashboard'
+  get 'impact', to: 'pages#impact', as: 'impact'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :donees do
     resources :donations, only: %i[new create show]
   end
-  resources :reports, only: %i[new edit update show]
+  resources :reports, only: %i[new create edit update show]
 end
